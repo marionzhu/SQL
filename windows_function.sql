a7fb1f85dcab0e09ecdf990106b4406475d0d23a
Sijie (Marion) ZHU <sijie.zhu11@gmail.com>
2024-04-03 15:55:53 +0200

    Fetching

--- windows_function.sql
+++ windows_function.sql
@@ -75,3 +75,67 @@ SELECT
             ORDER BY Year ASC) AS Last_Champion
 FROM Athletics_Gold
 ORDER BY Event ASC, Gender ASC, Year ASC;
+
+
+
+-- Fetching, ranking, and paging
+-- Fetching
+
+-- lead 
+-- For each year, fetch the current gold medalist and the gold medalist 3 competitions ahead of the current row.
+WITH Discus_Medalists AS (
+  SELECT DISTINCT
+    Year,
+    Athlete
+  FROM Summer_Medals
+  WHERE Medal = 'Gold'
+    AND Event = 'Discus Throw'
+    AND Gender = 'Women'
+    AND Year >= 2000)
+
+SELECT
+  -- For each year, fetch the current and future medalists
+  year,
+  Athlete,
+  LEAD(Athlete,3) OVER (ORDER BY year ASC) AS Future_Champion
+FROM Discus_Medalists
+ORDER BY Year ASC;
+
+
+-- first value
+-- Return all athletes and the first athlete ordered by alphabetical order.
+WITH All_Male_Medalists AS (
+  SELECT DISTINCT
+    Athlete
+  FROM Summer_Medals
+  WHERE Medal = 'Gold'
+    AND Gender = 'Men')
+
+SELECT
+  -- Fetch all athletes and the first athlete alphabetically
+  Athlete,
+  FIRST_VALUE(Athlete) OVER (
+    ORDER BY Athlete ASC
+  ) AS First_Athlete
+FROM All_Male_Medalists;
+
+
+-- last value 
+-- Return the year and the city in which each Olympic games were held.
+-- Fetch the last city in which the Olympic games were held.
+WITH Hosts AS (
+  SELECT DISTINCT Year, City
+    FROM Summer_Medals)
+
+SELECT
+  Year,
+  City,
+  -- Get the last city in which the Olympic games were held
+  LAST_VALUE(City) OVER (
+   ORDER BY Year ASC
+   RANGE BETWEEN
+     UNBOUNDED PRECEDING AND
+     UNBOUNDED FOLLOWING
+  ) AS Last_City
+FROM Hosts
+ORDER BY Year ASC;
