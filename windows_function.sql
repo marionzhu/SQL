524e48e235281000a2723c683c46a49933af4d79
Sijie (Marion) ZHU <sijie.zhu11@gmail.com>
2024-04-04 11:10:33 +0200

    PIVOT TABLE

--- windows_function.sql
+++ windows_function.sql
@@ -391,3 +391,64 @@ FROM Country_Medals
 ORDER BY Country ASC, Year ASC;
 
 
+-- Pivot table
+-- A basic pivot
+-- You have the following table of Pole Vault gold medalist countries by gender in 2008 and 2012.
+-- Pivot it by Year to get the following reshaped, cleaner table.
+-- Create the correct extension.
+-- Fill in the column names of the pivoted table.
+-- Create the correct extension to enable CROSSTAB
+CREATE EXTENSION IF NOT EXISTS TABLEFUNC;
+
+SELECT * FROM CROSSTAB($$
+  SELECT
+    Gender, Year, Country
+  FROM Summer_Medals
+  WHERE
+    Year IN (2008, 2012)
+    AND Medal = 'Gold'
+    AND Event = 'Pole Vault'
+  ORDER By Gender ASC, Year ASC;
+-- Fill in the correct column names for the pivoted table
+$$) AS ct (Gender VARCHAR,
+           "2008" VARCHAR,
+           "2012" VARCHAR)
+
+ORDER BY Gender ASC;
+
+-- Pivoting with ranking
+-- You'll need to count the gold medals each country has earned, produce the ranks of each country by medals earned, then pivot the table to this shape.
+-- Pivot the query's results by Year by filling in the new table's correct column names.
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+  WITH Country_Awards AS (
+    SELECT
+      Country,
+      Year,
+      COUNT(*) AS Awards
+    FROM Summer_Medals
+    WHERE
+      Country IN ('FRA', 'GBR', 'GER')
+      AND Year IN (2004, 2008, 2012)
+      AND Medal = 'Gold'
+    GROUP BY Country, Year)
+
+  SELECT
+    Country,
+    Year,
+    RANK() OVER
+      (PARTITION BY Year
+       ORDER BY Awards DESC) :: INTEGER AS rank
+  FROM Country_Awards
+  ORDER BY Country ASC, Year ASC;
+-- Fill in the correct column names for the pivoted table
+$$) AS ct (Country VARCHAR,
+           "2004" INTEGER,
+           "2008" INTEGER,
+           "2012" INTEGER)
+
+Order by Country ASC;
+
+
+
