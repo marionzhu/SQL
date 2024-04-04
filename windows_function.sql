7d5a1f55513de290c92583917ac8c7cbd5f3a349
Sijie (Marion) ZHU <sijie.zhu11@gmail.com>
2024-04-04 15:11:19 +0200

    coalesce and string_agg

--- windows_function.sql
+++ windows_function.sql
@@ -486,4 +486,45 @@ GROUP BY CUBE(Gender, Medal)
 ORDER BY Gender ASC, Medal ASC;
 
 
+-- coalesce 
+-- clean up results
+-- Turn the nulls in the Country column to All countries, and the nulls in the Gender column to All genders.
+SELECT
+  -- Replace the nulls in the columns with meaningful text
+  COALESCE(Country, 'All countries') AS Country,
+  COALESCE(Gender, 'All genders') AS Gender,
+  COUNT(*) AS Awards
+FROM Summer_Medals
+WHERE
+  Year = 2004
+  AND Medal = 'Gold'
+  AND Country IN ('DEN', 'NOR', 'SWE')
+GROUP BY ROLLUP(Country, Gender)
+ORDER BY Country ASC, Gender ASC;
+
+-- STRING_AGG
+-- summarizing results
+-- Return the top 3 countries by medals awarded as one comma-separated string.
+WITH Country_Medals AS (
+  SELECT
+    Country,
+    COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE Year = 2000
+    AND Medal = 'Gold'
+  GROUP BY Country),
+
+  Country_Ranks AS (
+  SELECT
+    Country,
+    RANK() OVER (ORDER BY Medals DESC) AS Rank
+  FROM Country_Medals
+  ORDER BY Rank ASC)
+
+-- Compress the countries column
+SELECT STRING_AGG(Country, ', ')
+FROM Country_Ranks
+-- Select only the top three ranks
+WHERE Rank <= 3;
+
 
