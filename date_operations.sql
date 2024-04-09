997e93cbf31214a943a1a9f8fa04626b9422b4fc
Sijie (Marion) ZHU <sijie.zhu11@gmail.com>
2024-04-09 11:04:31 +0200

    date

---
+++ date_operations.sql
@@ -0,0 +1,54 @@
+-- Adding and subtracting date and time values
+-- Subtract the rental_date from the return_date to calculate the number of days_rented.
+SELECT f.title, f.rental_duration,
+    -- Calculate the number of days rented
+    r.return_date - r.rental_date AS days_rented
+FROM film AS f
+     INNER JOIN inventory AS i ON f.film_id = i.film_id
+     INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+ORDER BY f.title;
+
+-- Now use the AGE() function to calculate the days_rented.
+SELECT f.title, f.rental_duration,
+    -- Calculate the number of days rented
+	AGE(return_date, rental_date) AS days_rented
+FROM film AS f
+	INNER JOIN inventory AS i ON f.film_id = i.film_id
+	INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+ORDER BY f.title;
+
+
+
+-- INTERVAL arithmetic
+-- Convert rental_duration by multiplying it with a 1 day INTERVAL
+-- Subtract the rental_date from the return_date to calculate the number of days_rented.
+-- Exclude rentals with a NULL value for return_date.
+SELECT
+	f.title,
+ 	-- Convert the rental_duration to an interval
+    INTERVAL '1' day * f.rental_duration,
+ 	-- Calculate the days rented as we did previously
+    r.return_date - r.rental_date AS days_rented
+FROM film AS f
+    INNER JOIN inventory AS i ON f.film_id = i.film_id
+    INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+-- Filter the query to exclude outstanding rentals
+WHERE r.return_date IS NOT NULL
+ORDER BY f.title;
+
+-- Calculating the expected return date
+-- Convert rental_duration by multiplying it with a 1-day INTERVAL.
+-- Add it to the rental date.
+SELECT
+    f.title,
+	r.rental_date,
+    f.rental_duration,
+    -- Add the rental duration to the rental date
+    INTERVAL '1' day * f.rental_duration + r.rental_date AS expected_return_date,
+    r.return_date
+FROM film AS f
+    INNER JOIN inventory AS i ON f.film_id = i.film_id
+    INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+ORDER BY f.title;
+
+-- Using EXTRACT
