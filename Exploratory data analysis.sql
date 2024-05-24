ab3ba64355f217ad105745af3992dadd2644f67c
Sijie (Marion) ZHU <sijie.zhu11@gmail.com>
2024-05-24 11:23:58 +0200

    generate series

--- Exploratory data analysis.sql
+++ Exploratory data analysis.sql
@@ -84,6 +84,31 @@ SELECT trunc(employees, -4) AS employee_bin,
 
 
 -- GENERATE SERIES
+-- Summarize the distribution of the number of questions with the tag "dropbox" on Stack Overflow per day by binning the data.
+-- Bins created in Step 2
+WITH bins AS (
+      SELECT generate_series(2200, 3050, 50) AS lower,
+             generate_series(2250, 3100, 50) AS upper),
+     -- Subset stackoverflow to just tag dropbox (Step 1)
+     dropbox AS (
+      SELECT question_count 
+        FROM stackoverflow
+       WHERE tag='dropbox') 
+-- Select columns for result
+-- What column are you counting to summarize?
+SELECT lower, upper, count(question_count) 
+  FROM bins  -- Created above
+       -- Join to dropbox (created above), 
+       -- keeping all rows from the bins table in the join
+       LEFT JOIN dropbox
+       -- Compare question_count to lower and upper
+         ON question_count >= lower 
+        AND question_count < upper
+ -- Group by lower and upper to count values in each bin
+ GROUP BY lower, upper
+ -- Order by lower to put bins in order
+ ORDER BY lower;
 
+ 
 
 
