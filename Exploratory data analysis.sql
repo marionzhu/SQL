88c40cc676414202e692d500749b3beffb0dcf0a
Sijie (Marion) ZHU <sijie.zhu11@gmail.com>
2024-05-24 18:15:59 +0200

    compute correlations

--- Exploratory data analysis.sql
+++ Exploratory data analysis.sql
@@ -185,3 +185,38 @@ SELECT startdates.tag,
          AND so_max.date = '2018-09-25';
 
 
+
+
+-- Compute the correlations between each pair of profits, profits_change, and revenues_change from the Fortune 500 data.
+DROP TABLE IF EXISTS correlations;
+
+CREATE TEMP TABLE correlations AS
+SELECT 'profits'::varchar AS measure,
+       corr(profits, profits) AS profits,
+       corr(profits, profits_change) AS profits_change,
+       corr(profits, revenues_change) AS revenues_change
+  FROM fortune500;
+
+INSERT INTO correlations
+SELECT 'profits_change'::varchar AS measure,
+       corr(profits_change, profits) AS profits,
+       corr(profits_change, profits_change) AS profits_change,
+       corr(profits_change, revenues_change) AS revenues_change
+  FROM fortune500;
+
+INSERT INTO correlations
+SELECT 'revenues_change'::varchar AS measure,
+       corr(revenues_change, profits) AS profits,
+       corr(revenues_change, profits_change) AS profits_change,
+       corr(revenues_change, revenues_change) AS revenues_change
+  FROM fortune500;
+
+-- Select each column, rounding the correlations
+SELECT measure, 
+       ROUND(profits::numeric,2) AS profits,
+       ROUND(profits_change::numeric,2) AS profits_change,
+       ROUND(revenues_change::numeric,2) AS revenues_change
+  FROM correlations;
+  
+
+
