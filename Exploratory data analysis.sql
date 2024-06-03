a286cf427d6bd41e9337f2a2ca7de81f5d9b88d1
Sijie (Marion) ZHU <sijie.zhu11@gmail.com>
2024-06-03 16:29:37 +0200

    Splitting and concanate text

--- Exploratory data analysis.sql
+++ Exploratory data analysis.sql
@@ -254,3 +254,172 @@ SELECT measure,
 
 
 
+-- exploring catagorical data and unstructured text
+-- CASE AND SPACE 
+-- TRIM/ LTRIM/ RTRIM
+-- Trim digits 0-9, #, /, ., and spaces from the beginning and end of street.
+ SELECT distinct street,
+       -- Trim off unwanted characters from street
+       trim(street, '0123456789 #/.') AS cleaned_street
+  FROM evanston311
+ ORDER BY street;
+
+
+
+
+--Use ILIKE to count rows in evanston311 where the description contains 'trash' or 'garbage' regardless of case.
+SELECT  Count(*)
+  FROM evanston311
+ -- Where description includes trash or garbage
+ WHERE description ilike '%trash%'
+  OR description ilike '%garbage%';
+  
+
+-- Count rows where the description includes 'trash' or 'garbage' but the category does not.
+SELECT Count(*)
+  FROM evanston311 
+ -- description contains trash or garbage (any case)
+ WHERE (description ILIKE '%trash%'
+    OR description ILIKE '%garbage%') 
+ -- category does not contain Trash or Garbage
+   AND category NOT LIKE '%Trash%'
+   AND category NOT LIKE '%Garbage%';
+
+
+
+
+
+-- SPLITTING AND CONCATENATING TEXT
+-- -- Concatenate house_num, a space, and street
+-- and trim spaces from the start of the result
+SELECT LTRIM(CONCAT(house_num, ' ', street)) AS address
+  FROM evanston311;
+
+-- Use split_part() to select the first word in street; alias the result as street_name.
+-- Also select the count of each value of street_name.
+
+-- Select the first word of the street value
+SELECT SPLIT_PART(street, ' ', 1) AS street_name, 
+       count(*)
+  FROM evanston311
+ GROUP BY street_name
+ ORDER BY count DESC
+ LIMIT 20;
+
+
+ -- Select the first 50 chars when length is greater than 50
+SELECT CASE WHEN length(description) > 50
+            THEN LEFT(description, 50) || '...'
+       -- otherwise just select description
+       ELSE description
+       END
+  FROM evanston311
+ -- limit to descriptions that start with the word I
+ WHERE description LIKE 'I %'
+ ORDER BY description;
+
+
+
+
+
+ -- MULTIPLE TRASFORMATION
+ -- Create recode with a standardized column; use split_part() and then rtrim() to remove any remaining whitespace on the result of split_part().
+ -- Fill in the command below with the name of the temp table
+DROP TABLE IF EXISTS recode;
+
+-- Create and name the temporary table
+CREATE TEMP TABLE recode AS
+-- Write the select query to generate the table 
+-- with distinct values of category and standardized values
+  SELECT DISTINCT category, 
+         RTRIM(SPLIT_PART(category, '-', 1)) AS standardized
+    -- What table are you selecting the above values from?
+    FROM evanston311;
+
+-- Look at a few values before the next step
+SELECT DISTINCT standardized 
+  FROM recode
+ WHERE standardized LIKE 'Trash%Cart'
+    OR standardized LIKE 'Snow%Removal%';
+
+-- Update to group trash cart values
+UPDATE recode 
+   SET standardized='Trash Cart' 
+ WHERE standardized LIKE 'Trash%Cart';
+
+-- Update to group snow removal values
+UPDATE recode 
+   SET standardized='Snow Removal' 
+ WHERE standardized LIKE 'Snow%Removal%';
+    
+-- Examine effect of updates
+SELECT DISTINCT standardized 
+  FROM recode
+ WHERE standardized LIKE 'Trash%Cart'
+    OR standardized LIKE 'Snow%Removal%';
+
+-- Update to group unused/inactive values
+UPDATE recode 
+   SET standardized='UNUSED' 
+ WHERE standardized IN ('THIS REQUEST IS INACTIVE...Trash Cart',
+  '(DO NOT USE) Water Bill', 
+  'DO NOT USE Trash',
+  'NO LONGER IN USE');
+
+-- Examine effect of updates
+SELECT DISTINCT standardized 
+  FROM recode
+ ORDER BY standardized;
+
+ -- Select the recoded categories and the count of each
+SELECT recode.standardized, count(*)
+-- From the original table and table with recoded values
+  FROM evanston311
+       LEFT JOIN recode 
+       -- What column do they have in common?
+       ON evanston311.category = recode.category 
+ -- What do you need to group by to count?
+ GROUP BY recode.standardized
+ -- Display the most common val values first
+ ORDER BY count(*) DESC;
+
+
+
+
+ -- Create a table with indicator variables
+ -- Create a temp table indicators from evanston311 with three columns: id, email, and phone.
+-- To clear table if it already exists
+DROP TABLE IF EXISTS indicators;
+
+-- Create the indicators temp table
+CREATE TEMP TABLE indicators AS
+  -- Select id
+  SELECT id, 
+         -- Create the email indicator (find @)
+         CAST (description LIKE '%@%' AS integer) AS email,
+         -- Create the phone indicator
+         CAST (description LIKE '%___-___-____%' AS integer) AS phone 
+    -- What table contains the data? 
+    FROM evanston311;
+
+-- Inspect the contents of the new temp table
+SELECT *
+  FROM indicators;
+
+
+-- Select the column you'll group by
+SELECT priority,
+       -- Compute the proportion of rows with each indicator
+       SUM(email)/COUNT(*)::numeric AS email_prop, 
+       SUM(phone)/COUNT(*)::numeric AS phone_prop
+  -- Tables to select from
+  FROM evanston311
+       -- Joining condition
+       LEFT JOIN indicators
+       ON evanston311.id= indicators.id
+ -- What are you grouping by?
+ GROUP BY priority;
+
+
+ 
+
