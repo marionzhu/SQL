38d7adf791aa12db0066be7d239c298265950a36
Sijie (Marion) ZHU <sijie.zhu11@gmail.com>
2025-05-07 15:46:09 +0200

    test

---
+++ test.sql
@@ -0,0 +1,29 @@
+-- test 
+SELECT CATEGORY_NAME, sum(PRICE *QUANTITY)/sum(QUANTITY)
+FROM business_value as bv
+left join dim_product as dp
+    on bv.PRODUCT_ID = dp.PRODUCT_ID and bv.ORDER_DATE BETWEEN dp.DATE_FROM and dp.DATE_TO
+left join dim_category as dc
+    on dp.LOWEST_LEVEL_ID = dc.LOWEST_LEVEL_ID
+where extract(year from bv.ORDER_DATE) = 2024 
+group by CATEGORY_NAME;
+
+
+
+SELECT distinct FAMILY_NAME, sum(BV_EURO) over(partition by FAMILY_NAME)/ sum(BV_EURO) over () as pec,
+FROM business_value as bv
+left join dim_product as dp
+    on bv.PRODUCT_ID = dp.PRODUCT_ID and bv.ORDER_DATE BETWEEN dp.DATE_FROM and dp.DATE_TO
+left join dim_category as dc
+    on dp.LOWEST_LEVEL_ID = dc.LOWEST_LEVEL_ID
+where extract(year from bv.ORDER_DATE) = 2024 
+-- group by FAMILY_NAME;
+
+
+select count(distinct case when bv2.TRANSACTION_ID is not null then bv1.TRANSACTION_ID end) / count(distinct bv1.TRANSACTION_ID)
+from business_value as bv1 
+leftjoin business_value as bv2 
+    on bv1.USER_ID = bv2.USER_ID
+where datediff(bv1.ORDER_DATE, bv2.ORDER_DATE) between 1 and  365 
+    and year(bv1.ORDER_DATE) = 2023
+    and year(bv2.ORDER_DATE) >= 2023
